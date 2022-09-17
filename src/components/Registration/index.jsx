393b54904760a35cbaeee5fcbb4b8991f34a3ab5
mhrabovska-vccp <mhrabovska-vccp.com>
2022-09-17 19:58:58 +0200

    Transform to one state only

--- src/components/Registration/index.jsx
+++ src/components/Registration/index.jsx
@@ -1,71 +1,105 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './style.css';
 
 const Registation = () => {
 
-    // const [user, setUser] = useState({
-    //     email: "",
-    //     username: "",
-    //     password: "",
-    //     passwordConfirm: "",
-    //   });
+    const [user, setUser] = useState({
+        email: "",
+        username: "",
+        password: "",
+        passwordConfirm: "",
+      });
 
-    const [email, setEmail] = useState(null);
-    const [username, setUsername] = useState(null);
-    const [password,setPassword] = useState(null);
-    const [confirmPassword,setConfirmPassword] = useState(null);
+    // const [email, setEmail] = useState(null);
+    // const [username, setUsername] = useState(null);
+    // const [password,setPassword] = useState(null);
+    // const [confirmPassword,setConfirmPassword] = useState(null);
 
-    const handleInputChange = (e) => {
-        const {id , value} = e.target;
-        if(id === "email"){
-            setEmail(value);
-        }
-        if(id === "name"){
-            setUsername(value);
-        }
-        if(id === "password"){
-            setPassword(value);
-        }
-        if(id === "confirmPassword"){
-            setConfirmPassword(value);
-        }
+    // const handleInputChange = (e) => {
+    //     const {id , value} = e.target;
+    //     if(id === "email"){
+    //         setEmail(value);
+    //     }
+    //     if(id === "name"){
+    //         setUsername(value);
+    //     }
+    //     if(id === "password"){
+    //         setPassword(value);
+    //     }
+    //     if(id === "confirmPassword"){
+    //         setConfirmPassword(value);
+    //     }
 
-    }
+    // }
+
+    useEffect(() => {
+        if (user.email.indexOf("@") >= 0 && user.username.length === 0) {
+          setUser({ ...user, username: user.email.substring(0, user.email.indexOf("@")) });
+        }
+      }, [user.email]);
+    
 
     const handleSubmit = () => {
-        console.log({email,username,password,confirmPassword});
+        console.log(user);
+        // console.log({email,username,password,confirmPassword});
     }
 
 
      return (
         <>
         <div className="header">
-            <h1 className="formHeadline">REGISTRATION</h1>
+            <h1 className="header-headline">REGISTRATION</h1>
         </div>
 
         <div className="form">
             <form className="form-content">
                 <div className="email">
                     <label className="form__label" htmlFor="email" hidden>Email Address </label>
-                    <input className="form__input" type="email" id="email" name="email" onChange = {handleInputChange} placeholder="Email Address"/>
+                    <input
+                        className="form__input"
+                        type="email" id="email"
+                        name="email"
+                        alue={user.email} 
+                        onChange={(e) => setUser({ ...user, email: e.target.value })} 
+                        placeholder="Email Address"/>
                 </div>
                 <div className="username">
-                    <label className="form__label" htmlFor="name" hidden>User Name </label>
-                    <input className="form__input" type="text" id="name" name="name" onChange = {handleInputChange} placeholder="User Name"/>
+                    <label className="form__label" htmlFor="username" hidden>User Name </label>
+                    <input 
+                        className="form__input"
+                        type="text"
+                        id="username"
+                        name="username"
+                        value={user.username}
+                        onChange={(e) => setUser({ ...user, username: e.target.value })}
+                        placeholder="User Name"/>
                 </div>
                 <div className="password">
                     <label className="form__label" htmlFor="password" hidden>Password </label>
-                    <input className="form__input" type="password" id="password" name="password" onChange = {handleInputChange}  placeholder="Password"/>
+                    <input 
+                        className="form__input"
+                        type="password" 
+                        id="password" 
+                        name="password"
+                        value={user.password}
+                        onChange={(e) => setUser({ ...user, password: e.target.value })}
+                        placeholder="Password"/>
                 </div>
                 <div className="confirm-password">
                     <label className="form__label" htmlFor="confirmPassword" hidden>Confirm Password </label>
-                    <input className="form__input" type="password" id="confirmPassword" name="confirmPassword" onChange = {handleInputChange} placeholder="Confirm Password"/>
+                    <input 
+                        className="form__input" 
+                        type="password" 
+                        id="confirmPassword" 
+                        value={user.passwordConfirm}
+                        onChange={(e) => setUser({ ...user, passwordConfirm: e.target.value })}
+                        placeholder="Confirm Password"/>
                 </div>
             </form>
         </div>
 
         <div className="footer">
-            <button className="btn" type="submit" onClick={handleSubmit}>REGISTER</button>
+            <button className="button" type="submit" onClick={handleSubmit}>REGISTER</button>
         </div>
         </>
      )
