c8a7c4a3c378c94a161956124a9f6d375e96d602
mhrabovska-vccp <mhrabovska-vccp.com>
2022-09-17 18:00:39 +0200

    Use 4 different states

--- src/components/Registration/index.jsx
+++ src/components/Registration/index.jsx
@@ -3,16 +3,37 @@ import './style.css';
 
 const Registation = () => {
 
-    const [user, setUser] = useState({
-        email: "",
-        username: "",
-        password: "",
-        passwordConfirm: "",
-      });
+    // const [user, setUser] = useState({
+    //     email: "",
+    //     username: "",
+    //     password: "",
+    //     passwordConfirm: "",
+    //   });
 
+    const [email, setEmail] = useState(null);
+    const [username, setUsername] = useState(null);
+    const [password,setPassword] = useState(null);
+    const [confirmPassword,setConfirmPassword] = useState(null);
 
-    const handleClick = () => {
-        console.log(user);
+    const handleInputChange = (e) => {
+        const {id , value} = e.target;
+        if(id === "email"){
+            setEmail(value);
+        }
+        if(id === "name"){
+            setUsername(value);
+        }
+        if(id === "password"){
+            setPassword(value);
+        }
+        if(id === "confirmPassword"){
+            setConfirmPassword(value);
+        }
+
+    }
+
+    const handleSubmit = () => {
+        console.log({email,username,password,confirmPassword});
     }
 
 
@@ -26,25 +47,25 @@ const Registation = () => {
             <form className="form-content">
                 <div className="email">
                     <label className="form__label" htmlFor="email" hidden>Email Address </label>
-                    <input className="form__input" type="email" id="email" name="email" placeholder="Email Address"/>
+                    <input className="form__input" type="email" id="email" name="email" onChange = {handleInputChange} placeholder="Email Address"/>
                 </div>
                 <div className="username">
                     <label className="form__label" htmlFor="name" hidden>User Name </label>
-                    <input className="form__input" type="text" id="name" name="name" placeholder="User Name"/>
+                    <input className="form__input" type="text" id="name" name="name" onChange = {handleInputChange} placeholder="User Name"/>
                 </div>
                 <div className="password">
                     <label className="form__label" htmlFor="password" hidden>Password </label>
-                    <input className="form__input" type="password" id="password" name="password" placeholder="Password"/>
+                    <input className="form__input" type="password" id="password" name="password" onChange = {handleInputChange}  placeholder="Password"/>
                 </div>
                 <div className="confirm-password">
                     <label className="form__label" htmlFor="confirmPassword" hidden>Confirm Password </label>
-                    <input className="form__input" type="password" id="confirmPassword" name="confirmPassword" placeholder="Confirm Password"/>
+                    <input className="form__input" type="password" id="confirmPassword" name="confirmPassword" onChange = {handleInputChange} placeholder="Confirm Password"/>
                 </div>
             </form>
         </div>
 
         <div className="footer">
-            <button className="btn" type="submit" onClick={handleClick}>REGISTER</button>
+            <button className="btn" type="submit" onClick={handleSubmit}>REGISTER</button>
         </div>
         </>
      )
